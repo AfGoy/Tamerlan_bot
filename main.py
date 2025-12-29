911196a448e1989a92fef9c8cad00a640724c34b
dotnetx86 <dotnetx86@gmail.com>
2025-12-29 23:59:35 +0300

    reworked addquote

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 load_dotenv()
 
@@ -55,32 +56,48 @@ async def quote_handler(message: Message):
 
         await message.answer(f"Вот ваша цитата Тамерлана: \n{quote.text}")
 
+
+
+class RequestCallback(CallbackData, prefix="request"):
+    action: str
+    quote: str
+    user_id: int
+
 @dp.message(Command("addquote"))
 async def quote_handler(message: Message):
-    full_text = message.text
+    args = message.text.strip().split(maxsplit=1)
+    
+    if len(args) != 2:
+       await message.answer("Цитата не может быть пустой. Пожалуйста, введите текст цитаты после команды /addquote.")
+       return
+    
+    quote = args[1]
+        
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Принять",
+                    callback_data=RequestCallback(action="accept", quote=quote, user_id=message.from_user.id).pack()
+                ),
+                InlineKeyboardButton(
+                    text="Отклонить",
+                    callback_data=RequestCallback(action="deny", quote=quote, user_id=message.from_user.id).pack()
+                ),
+            ]
+        ]
+    )
+    
+    await bot.send_message(ADMIN_ID, f"Запрос на добавление цитаты: {html.quote(quote)}", reply_markup=keyboard)
+    
+    # if message.from_user.id == int(ADMIN_ID):
+    #     if not quote_text:
+    #         await message.answer("Цитата не может быть пустой. Пожалуйста, введите текст цитаты после команды /addquote.")
+    #         return
 
-    command = "/addquote@Tamerlan1488_bot"
-    if full_text.startswith(command):
-        quote_text = full_text[len(command):].strip()
-    else:
-        quote_text = full_text.split(maxsplit=1)[1] if len(full_text.split()) > 1 else ""
-
-    if message.from_user.id == int(ADMIN_ID):
-        if not quote_text:
-            await message.answer("Цитата не может быть пустой. Пожалуйста, введите текст цитаты после команды /addquote.")
-            return
-
-        async with AsyncSessionLocal() as session:
-            try:
-                async with session.begin():
-                    quote = Quote(text=quote_text)
-                    session.add(quote)
-                await message.answer(f"Цитата добавлена: {html.quote(quote_text)}")
-            except SQLAlchemyError as e:
-                logging.error(f"Ошибка базы данных при добавлении цитаты: {e}")
-                await message.answer("Произошла ошибка при добавлении цитаты.")
-    else:
-        await message.answer("У вас нет прав для добавления цитат.")
+        
+    # else:
+    #     await message.answer("У вас нет прав для добавления цитат.")
 
 
 @dp.message()
@@ -89,7 +106,29 @@ async def echo_handler(message: Message):
     if (message.from_user.id == int(FIRST_ID) or message.from_user.id == int(SEC_ID)) and num == 67:
         await message.answer("Тамерлан успокойся!")
 
+@dp.callback_query(RequestCallback.filter())
+async def menu_callback_handler(
+    callback: CallbackQuery,
+    callback_data: RequestCallback
+):
+    if callback_data.action == "accept":
+        async with AsyncSessionLocal() as session:
+            try:
+                async with session.begin():
+                    quote = Quote(text=callback_data.quote)
+                    session.add(quote)
+                await bot.send_message(ADMIN_ID, f"Добавлена новая цитата: {html.quote(callback_data.quote)}")
+            except SQLAlchemyError as e:
+                logging.error(f"Ошибка базы данных при добавлении цитаты: {e}")
+                await callback.answer("Произошла ошибка при добавлении цитаты.")
+
+    elif callback_data.action == "deny":
+        await callback.answer("Это цитата недостаточно тамерланская 😈")
+        await callback.message.delete()
+
 async def main() -> None:
+    global bot
+    
     await init_db()
 
     bot = Bot(token=TK, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
